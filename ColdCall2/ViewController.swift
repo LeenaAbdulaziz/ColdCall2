46d9fbad4bcf80fbd21e4207d03e958abb9c3d8d
linah_abdulaziz <linah.Abdulaziz@gmail.com>
2021-12-02 11:02:25 +0300

    Initial Commit

---
+++ ColdCall2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ColdCall2
+//
+//  Created by Linah abdulaziz on 27/04/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
