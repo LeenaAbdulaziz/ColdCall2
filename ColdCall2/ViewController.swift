461363a8929c5611f10843f4687ff93754135d8a
linah_abdulaziz <linah.Abdulaziz@gmail.com>
2021-12-05 10:46:30 +0300

    ColdCall2

--- ColdCall2/ViewController.swift
+++ ColdCall2/ViewController.swift
@@ -17,21 +17,40 @@ class ViewController: UIViewController {
     
     let list: [String] = ["Uyanga", "Courtny" ,"Jay" , "Bryant", "Jimmy" , "Cody", "Ryota"]
     
-    var randomName=Int.random(in: 0...6)
-    var randomNumber=Int.random(in: 1...5)
     
     
    
     @IBAction func OnCallPressed(_ sender: UIButton) {
-        //  if(randomNumber = 1 || randomNumber = 2){
-         
-        if (randomName<list.count-1){
-            randomName+=1
-        }else{
-            randomName=Int.random(in: 0...5)
-        }
-      nameLabel.text=list[randomName]
-        ChanedColor()
+        
+        var randomName = Int.random(in: 1...5)
+       
+        nameLabel.text=list.randomElement()
+        numberLabel.text=String(randomName)
+        
+        
+        if (randomName<6){
+                if (randomName == 1 || randomName == 2){
+                    numberLabel.textColor = UIColor.red
+                    numberLabel.text = String(randomName)
+        
+        
+                } else if (randomName == 3 || randomName == 4){
+                    numberLabel.textColor = UIColor.orange
+                    numberLabel.text = String(randomName)
+        
+        
+                } else if (randomName == 5 ){
+                    numberLabel.textColor = UIColor.green
+                    numberLabel.text = String(randomName)
+        
+                }
+                   
+        
+                }
+                else{
+                    randomName=Int.random(in: 1...5)
+                }
+       
         
        
     }
@@ -43,34 +62,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        //nameLabel.text="Ready?"
+        nameLabel.text="Ready?"
     }
     
-    func ChanedColor(){
-        if (randomNumber<6){
-        if (randomNumber == 1 || randomNumber == 2){
-            numberLabel.textColor = UIColor.red
-            numberLabel.text = String(randomNumber)
-           
-        } else if (randomNumber == 3 || randomNumber == 4){
-            numberLabel.textColor = UIColor.orange
-            numberLabel.text = String(randomNumber)
-            
-           
-        } else if (randomNumber == 5 ){
-            numberLabel.textColor = UIColor.green
-            numberLabel.text = String(randomNumber)
-            
-        }
-            randomNumber += 1
-        
-        }
-        else{
-            randomNumber=Int.random(in: 1...6)
-        }
-    }
-    
-    
-
-
 }
